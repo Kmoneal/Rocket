563eedae4b7f2855c18310465346722804270c31
Kmoneal <kmacjoneal@gmail.com>
2018-07-17 13:35:45 -0700

    Add method to LocalRequest, allowing a certificate to be added.

--- core/lib/src/local/request.rs
+++ core/lib/src/local/request.rs
@@ -5,7 +5,7 @@ use std::ops::{Deref, DerefMut};
 
 use {Request, Response, Data};
 use local::Client;
-use http::{Header, Cookie};
+use http::{Header, Cookie, tls::Certificate};
 
 /// A structure representing a local request as created by [`Client`].
 ///
@@ -269,6 +269,15 @@ impl<'c> LocalRequest<'c> {
     #[inline]
     pub fn private_cookie(self, cookie: Cookie<'static>) -> Self {
         self.request.cookies().add_original_private(cookie);
+        self
+    }
+
+    /// Add a certificate to this request.
+    pub fn certificate(mut self, cert: Certificate) -> Self {
+        let mut peer_certs = Vec::new();
+        peer_certs.push(cert);
+        self.request_mut().set_peer_certificates(peer_certs);
+
         self
     }
 
